cdf2b7ac204c8644d952bafffb114c5c5f700284
klogger <ValeraSadoev2009@gmail.com>
2025-12-23 10:20:09 +0300

    Create D.cpp

---
+++ yandex2024/region/tour1/D.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x.size())
+
+void solve() {
+
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
