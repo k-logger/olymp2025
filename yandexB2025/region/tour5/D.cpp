56b83590c975dedaf90e773580bd5546ce3c1666
klogger <ValeraSadoev2009@gmail.com>
2025-12-23 10:20:23 +0300

    Create D.cpp

---
+++ yandexB2025/region/tour5/D.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define sz(x) (int)(x.size())
+#define all(x) x.begin(), x.end()
+const int INF = (int)(1e10);
+void solve() {
+
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
