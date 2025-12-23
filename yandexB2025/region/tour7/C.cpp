b45c9a783c6db85af2d3e3e6d84aa1259f134c3f
klogger <ValeraSadoev2009@gmail.com>
2025-12-23 10:20:29 +0300

    Create C.cpp

---
+++ yandexB2025/region/tour7/C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x.size())
+#define ld long double
+
+void solve() {
+
+}
+
+signed main() {
+    int t = 1;
+    while (t--) solve();
+}
